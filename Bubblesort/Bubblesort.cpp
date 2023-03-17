609532ac704746da8804f0c51612a2523a26c456
agilimam <agilimamalhafizi04@gmail.com>
2023-03-17 16:15:45 +0700

    procedur algoritma array

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int a[20]			// Deklarasi array a dengan ukuran 20
+int a[20];			// Deklarasi array a dengan ukuran 20
 int n;				// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
+void input() {		// procedur untuk input 
+	while (true) { // Looping
+		cout << "masukkan banyaknya elemen pada array : ";	// Output ke layar
+		cin >> n;	// input dari pengguna
+		if (n <= 20)// jika n kurang dari atau sama dengan 20
+			break;  // keluar dari loop
+		else {		// jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+		}
+	}
+	cout << endl;								// Output baris kosong
+	cout << "====================" << endl;		// Output ke layar
+	cout << "Masukkan Elemen Array" << endl;	// Output ke layar
+	cout << "====================" << endl;		// Output ke layar
+
+	for (int i = 0; i < n; i++){				// Looping denga i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ":";	// Output ke layar
+		cin >> a[i];							// input dari pengguna
+	}
+}
